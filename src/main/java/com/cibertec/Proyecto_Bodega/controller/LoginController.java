8fbc660e4ef8795ca790fbeab0c132f55748f1f4
EJRazor <erickju01@gmail.com>
2023-06-24 22:51:44 -0400

    Avnce Marca y login controller

---
+++ src/main/java/com/cibertec/Proyecto_Bodega/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.cibertec.Proyecto_Bodega.controller;
+
+import com.cibertec.Proyecto_Bodega.model.bd.Usuario;
+import com.cibertec.Proyecto_Bodega.servicio.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class LoginController {
+    @Autowired
+    private UsuarioService usuarioService;
+    @GetMapping("/login")
+    public String login(){
+        return "Auth/frmLogin";
+    }
+    @GetMapping("/registrar")
+    public String registrar(){
+        return "Auth/frmRegistroUsuario";
+    }
+
+    @PostMapping("/guardarUsuario")
+    public String guardarUsuario(@ModelAttribute Usuario usuario){
+        usuarioService.guardarUsuario(usuario);
+        return "Auth/login";
+    }
+}
