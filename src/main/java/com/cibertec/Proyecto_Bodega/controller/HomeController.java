430a51080215d3780cb2865a85da4df74166d201
EJRazor <erickju01@gmail.com>
2023-07-01 02:10:08 -0400

    Fase de diseño -> Navbar creada

--- src/main/java/com/cibertec/Proyecto_Bodega/controller/HomeController.java
+++ src/main/java/com/cibertec/Proyecto_Bodega/controller/HomeController.java
@@ -1,7 +1,16 @@
 package com.cibertec.Proyecto_Bodega.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
+
+    @GetMapping("/inicio")
+    public String cargarHome(){
+        return "Home";
+    }
+
 }
